ef4c7c4b084f595faae8a60917a25fdc3e122222
silivalleydev <silivalleydev@gmail.com>
2021-11-02 21:50:48 +0900

    comment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,5 +10,7 @@ export const reducers = combineReducers({
 
 // yield 키워드 뒤에 리덕스 사가의 부수효과 함수가 온다.
 export function* rootSaga() {
+    // 매개변수로 전달된 함수를 비동기적으로 실행한다 
+    // 비동기란 쉽게 말하면그 함수를 누군가에게 대신 실행하게 맡기고 나는 다른일을 하러 떠나는 것이다.
   yield all([fork(userSaga)]);
 }
--- src/reducers/user/saga.js
+++ src/reducers/user/saga.js
@@ -7,8 +7,13 @@ function getUsers(params = "") {
 }
   
 function* searchRequest(action) {
+    // Call()은 함수를 동기적으로 실행한다 
+    // Call에 넘겨진 함수가 Promise를 리턴하면 그 Promise가 resolved 될 때까지 call()을 호출한 부분에서 실행이 멈춘다
     const userData = yield call(getUsers, action.params);
     try {
+        // Put() 액션을 dispatch 한다. 
+        // 보통 take로 액션을 캐치해서 api 호출을 call로 실행하고 성공/실패 여부에 따라 
+        // 리덕스 스토어에 반영하기 위해서 호출하는 Effects이다
         yield put({ type: SEARCH_USERS_SUCCESS, data: userData.data });
     } catch (err) {
         yield put({ type: SEARCH_USERS_FAIL, error: err.response.data });
@@ -17,10 +22,12 @@ function* searchRequest(action) {
   
   // 로그인 요청이 들어오는지를 감지하는 제너레이터 함수
   // addEventListener 함수와 그 사용법이 비슷하다.
+  // takeLatest 가장 마지막에(최신) 실행된 액션에 대해서만 핸들러를 실행한다
 function* waitSearchReq() {
     yield takeLatest(SEARCH_USERS_REQ, searchRequest);
 }
 
 export default function* userSaga() {
+    // all 함수 내부 배열에 등록된 사가 함수들을 리덕스 사가 미들웨어에 등록하는 부수효과 함수
     yield all([waitSearchReq()]);
 }
